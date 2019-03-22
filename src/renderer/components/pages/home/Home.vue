42255fd5f66b190d91788032b0a97915a3da3aad
SXPanda <jorjbr2@hotmail.co.uk>
2019-03-22 12:20:17 +0000

    Initial commit

---
+++ src/renderer/components/pages/home/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>Home</h1>
+    <el-button
+      type="primary"
+      @click="testFn">
+      Primary
+    </el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  methods: {
+    testFn() {
+      console.log('butt soup');
+      ipcRenderer.send('test-message', 'ping');
+    },
+  },
+};
+</script>
+
+<style>
+
+</style>
